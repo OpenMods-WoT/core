5625cd7b5834b9f658ed77c0e640814de2ad9a47
Kurzdor <github@kurzdor.me>
2024-09-03 20:45:30 +0300

    fix: upstream blurBackgroundViews fix for battle dialogs (not sure whether it's still necessary)

--- source/scripts/client/OpenModsCore/utils/delayed/utils/dialogs.py
+++ source/scripts/client/OpenModsCore/utils/delayed/utils/dialogs.py
@@ -1,4 +1,5 @@
 from gui.shared.view_helpers.blur_manager import _BlurManager
+from realm import CURRENT_REALM
 from ... import overrideMethod
 
 __all__ = ()
@@ -8,4 +9,7 @@ __all__ = ()
 def _setLayerBlur(base, self, blur, *args, **kwargs):
     if self._battle is None:
         return base(self, blur, *args, **kwargs)
-    self._battle.blurBackgroundViews(blur.ownLayer, blur.blurAnimRepeatCount)
+    if CURRENT_REALM != 'RU':
+        self._battle.blurBackgroundViews(blur.ownLayer, blur.blurAnimRepeatCount, blur.uiBlurRadius)
+    else:
+        self._battle.blurBackgroundViews(blur.ownLayer, blur.blurAnimRepeatCount)
